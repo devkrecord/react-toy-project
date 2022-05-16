a158fa7f9dbaba8b54b585b24152078772d4021b
dev.k <dev.krecord@gmail.com>
2022-05-16 19:23:22 +0900

    comment: useState 유의사항

--- basic/src/AppCounter.jsx
+++ basic/src/AppCounter.jsx
@@ -11,6 +11,23 @@ export default function AppCounter() {
         onClick={() => {
           setCount(count + 1);
         }}
+        /*
+        setState를 여러번 호출하거나, 비동기적으로 내가 예상하지 못한 순간 set할 경우 두번째 방법 사용
+        
+        1. 첫번째
+        onClick={() => {
+          setCount(count + 1);
+          setCount(count + 1);
+          setCount(count + 1);
+        }} 최종 count 값 0 
+
+        2. 두번쨰
+        onClick={() => {
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+        }} 최종 count 값 3
+        */
       >
         Add +
       </button>
