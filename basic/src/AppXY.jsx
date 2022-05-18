e0009e4c6e7c1a21744e15d5635b477e946d2c04
dev.k <dev.krecord@gmail.com>
2022-05-18 20:27:40 +0900

    refector: 마우스 따라가기 개선  (상태를 객체 단위로 보관 및 업데이트)

--- basic/src/AppXY.jsx
+++ basic/src/AppXY.jsx
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import "./AppXY.css";
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  // 서로 연관있는 데이터라면 ( 서로 밀접하고 업데이트가 동시에 일어나는 경우 ) 객체로 묶어서 관리하는것이 효율적
+  // 상태를 객체 단위로 보관하고 업데이트
+  const [position, setPosition] = useState({ x: 0, y: 0 });
   return (
     <div
       className="container"
       onPointerMove={(e) => {
-        setX(e.clientX);
-        setY(e.clientY);
+        // setPosition({ x: e.clientX, y: e.clientY });
+        // 만약 수평으로만 이동이 가능하다면?
+        setPosition((prev) => ({ x: e.clientX, y: prev.y }));
       }}
     >
       <div
         className="pointer"
-        style={{ transform: `translate(${x}px, ${y}px)` }}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
       ></div>
     </div>
   );
