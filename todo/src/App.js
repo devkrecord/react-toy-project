a98669b4d6c21f024c9b409d56dc06e9c6425e7c
dev.k <dev.krecord@gmail.com>
2022-05-26 19:41:19 +0900

    feat: 리스트 상태에 따른 필터 기능 구현

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { useState } from 'react';
 import TodoList from './components/TodoList/TodoList';
+import Header from './components/Header/Header';
 
+// 리액트는 단방향 데이터 흐름이므로 공통된 데이터는 가장 근접한 부모 컴포넌트에 배치
+const filters = ['all', 'active', 'completed'];
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
   return (
     <div>
-      <TodoList />
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </div>
   );
 }
--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: '123', text: '장보기', status: 'active' },
     { id: '234', text: '공부하기', status: 'active' },
@@ -16,10 +16,12 @@ export default function TodoList() {
     setTodos(todos.filter((item) => item.id !== deleted.id));
   };
 
+  const filtered = getFilteredItems(todos, filter);
+
   return (
     <section>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
@@ -31,4 +33,13 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+
+// 내부의 다른 상태와 밀접하게 연관 있는 함수가 아니라면, 외부에 선언하는게 메모리 측면에서 더 낫다.
+// 컴포넌트 내부에 작성하게 되면 컴포넌트가 re-render 될때마다 계속 불필요하게 재선언(재할당)되기 때문
+function getFilteredItems(todos, filter) {
+  if (filter === 'all') {
+    return todos;
+  }
+  return todos.filter((todos) => todos.status === filter);
 }
