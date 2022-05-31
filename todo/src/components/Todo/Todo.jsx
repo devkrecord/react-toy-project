672c7e5fe3e678354236483843168bbcddeb5de7
dev.k <dev.krecord@gmail.com>
2022-05-31 19:35:42 +0900

    design: 투두 아이템 스타일링

--- todo/src/components/Todo/Todo.jsx
+++ todo/src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa'; // react-icons/접두사 <- 'Fa'TrashAlt
+import styles from './Todo.module.css';
 
 // $ yarn add react-icons 라이브러리 설치
 export default function Todo({ todo, onUpdate, onDelete }) {
@@ -12,18 +13,23 @@ export default function Todo({ todo, onUpdate, onDelete }) {
     onDelete(todo);
   };
   return (
-    <li>
+    <li className={styles.todo}>
       <input
+        className={styles.checkbox}
         type="checkbox"
         id={id}
         checked={status === 'completed'}
         onChange={handleChange}
       />
       {/*  htmlFor에 input의 아이디나 네임을 적어 input과 연결 할 수 있다. */}
-      <label htmlFor={id}>{text}</label>
-      <button onClick={handleDelete}>
-        <FaTrashAlt />
-      </button>
+      <label htmlFor={id} className={styles.text}>
+        {text}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+          <FaTrashAlt />
+        </button>
+      </span>
     </li>
   );
 }
