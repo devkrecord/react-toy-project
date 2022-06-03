5a753cfbf9c7c3ecc4bc04d1763ce05482627e1a
dev.k <dev.krecord@gmail.com>
2022-06-03 22:45:12 +0900

    feat: 다크모드 셋팅 기억 (로컬스토리지)

--- todo/src/context/DarkModeContext.jsx
+++ todo/src/context/DarkModeContext.jsx
@@ -1,13 +1,25 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const DarkModeContext = createContext();
 
 export default function DarkModeProvider({ children }) {
   const [darkMode, setDarkMode] = useState(false);
+
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
     updateDarkMode(!darkMode);
   };
+
+  // provider가 처음 마운트되었을 때 다크모드인지 아닌지 판단 후 초기값 셋팅
+  useEffect(() => {
+    const isDark =
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
+
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
       {children}
@@ -19,8 +31,10 @@ export default function DarkModeProvider({ children }) {
 function updateDarkMode(darkMode) {
   if (darkMode) {
     document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
   } else {
     document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
   }
 }
 
